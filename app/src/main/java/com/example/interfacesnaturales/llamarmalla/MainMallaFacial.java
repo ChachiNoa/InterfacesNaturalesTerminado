0a747d71df551b8d8196e2c2a49fe8a13ca5e81f
Ainhoa <agutgar3003@g.educaand.es>
2025-11-19 02:18:58 +0100

    completamente funcional y listo para descargar. Nota: El audio de activacion y finalizacion de captura de audio está desactivada

--- app/src/main/java/com/example/interfacesnaturales/llamarmalla/MainMallaFacial.java
+++ app/src/main/java/com/example/interfacesnaturales/llamarmalla/MainMallaFacial.java
@@ -181,7 +181,6 @@ public class MainMallaFacial extends AppCompatActivity {
             public void onEvent(int eventType, Bundle params) {}
         });
     }
-
     private void checkAndRequestAudioPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_AUDIO_PERMISSION_CODE);
